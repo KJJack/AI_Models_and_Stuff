89a05c61d9cf6b91bd8bdd63e371d386aad23251
robert-gomez <roberto.gomez05@utrgv.edu>
2021-04-15 19:44:34 -0500

    robert files

--- gomokuAI.py
+++ gomokuAI.py
@@ -13,6 +13,9 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.backend import reshape
 from keras.utils.np_utils import to_categorical
+import pandas as pd
+import numpy as np
+import csv
 
 #Creates board, all empty spots wiht zeros
 def initBoard():
@@ -86,7 +89,7 @@ def getWinner(board):
             if candidate > 0 and i <= len(board) - 5 and j <= len(board) - 5:
                 for d in range(0,5):
                 # Make sure there are no gaps
-                    if i + d > len(board) or j + d > len(board) or candidate != board[i + d][j + d]:
+                    if i + d >= len(board) or j + d > len(board) or candidate != board[i + d][j + d]:
                         break
                     else:
                         how_many_so_far += 1
@@ -95,10 +98,12 @@ def getWinner(board):
                         won = candidate
                         return won
 
-                how_many_so_far = 0
+
+            how_many_so_far = 0
+            if candidate > 0 and i <= len(board):
                 for d in range(0, 5):
                     # Make sure there are no gaps
-                    if  i + d > len(board) or j - d < 0 or candidate != board[i + d][j - d]:
+                    if  i + d >= len(board) or j - d < 0 or candidate != board[i + d][j - d]:
                         break
                     else:
                         how_many_so_far += 1
@@ -128,11 +133,23 @@ def printBoard(board):
                 mark = 'X'
             elif board[i][j] == 2:
                 mark = 'O'
+            elif board[i][j] == 3:
+                mark = '1'
+            elif board[i][j] == 4:
+                mark = '2'
             if (j == len(board[i]) - 1):
                 print(mark)
             else:
                 print(str(mark) + "|", end='')
 
+
+def movesToBoard(moves):
+    board = initBoard()
+    for move in moves:
+        player = move[0]
+        coords = move[1]
+        board[coords[0]][coords[1]] = player
+    return board
 
 #outputs a list of moves made, "History" that when given to the next function generates the board state
 def simulateGame(p1=None, p2=None, rnd=0):
@@ -143,57 +160,48 @@ def simulateGame(p1=None, p2=None, rnd=0):
     tempList = []
     moves = getMoves(board)
     temp = None
-    rand = True
+    rand = False
     move2 = None
     move1 = None
     move = None
 
     # Moves keeps track of the available coordinates (x, y) left on the board
     # Board keeps track of the player occupation on board (0, 1, 2)
-    
-    '''
+
     while getWinner(board) == -1:
         move = None
         # Chosen move for player1
-        if playertoMove == 1:
-            move = getMove(board)
+        if playertoMove == 1 and rand == False:
+            moveList = trial.getBestMovesInAnArray(board)
+            move = rd.choice(moveList)
+        elif playertoMove == 1 and rand == True:
+            move = trial.getBestMove(board)
         elif playertoMove == 2 and rand == True:
-            move = getMove(board)
-        elif playertoMove == 2 and rand == False:
-            #import pdb; pdb.set_trace() n = nextline s = step in c = continue q = quit
-            move = bestMove(board)
+            # import pdb; pdb.set_trace() n = nextline s = step in c = continue q = quit
+            move = trial.getBestMove(board)
 
         # Player move now occupies board (0, 1, 2)
         board[move.x][move.y] = playertoMove
 
         # Append playertoMove and coordinates to history
-        history.append((playertoMove, (move.x, move.y)))
+        history.append([playertoMove, [move.x, move.y]])
 
         # Switch between player 1 and player 2
         playertoMove = 1 if playertoMove == 2 else 2
 
         # Swap AI approach
-        rand = True if rand == False else False
-        '''
-    
-    moveList = trial.getBestMovesInAnArray(board)
-    move = rd.choice(moveList)
+        rand = True if rand == False else True
 
-    print(f"{move.x} {move.y}")
-    #print(f"{move2.x} {move2.y}")
-   
+    # moveList = trial.getBestMovesInAnArray(board)
+    # move = rd.choice(moveList)
 
+    # print(f"{move.x} {move.y}")
+    # print(f"{move2.x} {move2.y}")
+    printBoard(movesToBoard(history))
+    print()
     return history
-
-
 #this function creates a board given a history from the previous simulated game function
-def movesToBoard(moves):
-    board = initBoard()
-    for move in moves:
-        player = move[0]
-        coords = move[1]
-        board[coords[0]][coords[1]] = player
-    return board
+
 
 
 
@@ -218,7 +226,7 @@ def movesToBoard(moves):
 # printBoard(board)
 # print("winner: " + str(getWinner(board)))
 
-'''
+
 #this function is for viewing stats from the list of a lot of games simulated
 def gameStats(games, player=1):
     stats = {"win": 0, "loss": 0, "draw": 0}
@@ -244,7 +252,7 @@ def gameStats(games, player=1):
     print("Wins: %d (%.1f%%)" % (stats["win"], winPct))
     print("Loss: %d (%.1f%%)" % (stats["loss"], lossPct))
     print("Draw: %d (%.1f%%)" % (stats["draw"], drawPct))
-'''
+
 '''
 def bestMove(board):
     combinations = []
@@ -345,14 +353,150 @@ def RePopulate(board, values, playerValues, combinations, playerCombinations):
 #gameStats(games)
 #print()
 #gameStats(games, player=2)
-#data = simulateGame()
-#print(data)
-board = movesToBoard(data)
-#printBoard(board)
-#print(getWinner(board))
-simulateGame()
-printBoard(board)
+def highlightWin(board):
+    candidate = 0
+    won = 0
+    winning_combination = []
+    # Check rows
+    for i in range(len(board)):
+        candidate = 0
+        how_many_so_far = 0
+        winning_combination = []
+        for j in range(len(board[i])):
+            # Make sure there are no gaps
+            if candidate != board[i][j]:
+                winning_combination = []
+                how_many_so_far = 1
+                candidate = board[i][j]
+                if(candidate > 0):
+                    winning_combination.append((i ,j))
+            else:
+                how_many_so_far += 1
+                if (candidate > 0):
+                    winning_combination.append((i, j))
 
+            # Determine whether the front-runner has all the slots
+            if how_many_so_far == 5 and candidate > 0:
+                won = candidate
+                for move in winning_combination:
+                    board[move[0]][move[1]] = candidate + 2
+                # print(candidate, how_many_so_far)
+            # print(how_many_so_far)
+    if won > 0:
+        return won
+
+    # Check columns
+    for j in range(len(board[0])):
+        candidate = 0
+        how_many_so_far = 0
+        winning_combination = []
+        for i in range(len(board)):
+
+            # Make sure there are no gaps
+            if candidate != board[i][j]:
+                winning_combination = []
+                how_many_so_far = 1
+                candidate = board[i][j]
+                if (candidate > 0):
+                    winning_combination.append((i, j))
+            else:
+                how_many_so_far += 1
+                if (candidate > 0):
+                    winning_combination.append((i, j))
+            # Determine whether the front-runner has all the slots
+            if how_many_so_far == 5 and candidate > 0:
+                won = candidate
+                for move in winning_combination:
+                    board[move[0]][move[1]] = candidate + 2
+
+    if won > 0:
+        return won
+
+    # Check diagonals
+    winning_combination = []
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            candidate = board[i][j]
+            how_many_so_far = 0
+            winning_combination = []
+            if candidate > 0 and i <= len(board) - 5 and j <= len(board) - 5:
+                for d in range(0, 5):
+                    # Make sure there are no gaps
+                    if i + d >= len(board) or j + d > len(board) or candidate != board[i + d][j + d]:
+                        break
+                    else:
+                        how_many_so_far += 1
+                        if (candidate > 0):
+                            winning_combination.append((i +d, j + d))
+                    # Determine whether the front-runner has all the slots
+                    if how_many_so_far == 5 and candidate > 0:
+                        won = candidate
+                        for move in winning_combination:
+                            board[move[0]][move[1]] = candidate + 2
+                        return won
+            winning_combination = []
+            how_many_so_far = 0
+            if candidate > 0 and i <= len(board):
+                for d in range(0, 5):
+                    # Make sure there are no gaps
+                    if i + d >= len(board) or j - d < 0 or candidate != board[i + d][j - d]:
+                        break
+                    else:
+                        how_many_so_far += 1
+                        if (candidate > 0):
+                            winning_combination.append((i+d, j-d))
+                    # Determine whether the front-runner has all the slots
+                    if how_many_so_far == 5 and candidate > 0:
+                        won = candidate
+                        for move in winning_combination:
+                            board[move[0]][move[1]] = candidate + 2
+                        return won
+                # print(candidate, how_many_so_far)
+            # print(how_many_so_far)
+    if won > 0:
+        return won
+
+    # Still no winner?
+    if (len(getMoves(board)) == 0):
+            # It's a draw
+        return 0
+    else:
+        # Still more moves to make
+        return -1
+
+
+#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+games = []
+for i in range(5000):
+    sim = simulateGame()
+    print(i)
+    games.append(sim)
+
+with open('simulated_games.csv', mode='w') as sim_games:
+    sim_games = csv.writer(sim_games, delimiter=',', escapechar=' ', quoting=csv.QUOTE_NONE)
+    for game in games:
+        sim_games.writerow(game)
+
+
+# gamesNP = np.array(games)
+# df = pd.DataFrame(gamesNP)
+# df.to_csv('sim_games.csv')
+
+# for i in games:
+#
+#     print(i)
+#     board = movesToBoard(i)
+#     printBoard(board)
+#     print(getWinner(board))
+#
+# gameStats(games)
+# print()
+# gameStats(games, player=2)
+
+
+# df = pd.read_csv('simulated_games.csv')
+# print(df)
 #now here we should write, train, test and print out the model results
 '''
 def getModel():
